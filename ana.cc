f01023931b245b0b6ebbd9f7723ca1d90f17a78a
Patrick Huck <phuck@lbl.gov>
2013-03-31 02:19:36 -0400

    initial commit

---
+++ ana.cc
@@ -0,0 +1,26 @@
+// Copyright (c) 2013 Patrick Huck
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[]) {
+
+  if ( argc < 1 ) return 1;
+
+  std::ifstream inFile;
+  inFile.open(argv[1]);
+  string line;
+  while ( getline(inFile, line) ) {
+    cout << line << endl;
+  }
+  inFile.close();
+
+  return 0;
+}
