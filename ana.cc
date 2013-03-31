d6ce235429ebeafab63dffa54d8ed332a8182532
Patrick Huck <phuck@lbl.gov>
2013-03-31 02:29:36 -0400

    test ana working

--- ana.cc
+++ ana.cc
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 
 using std::vector;
 using std::string;
@@ -16,11 +17,29 @@ int main(int argc, char *argv[]) {
 
   std::ifstream inFile;
   inFile.open(argv[1]);
+  std::ofstream outFile;
+  string fn = argv[1];
+  fn += ".out";
+  outFile.open(fn.c_str());
   string line;
+  int nl = -1;
   while ( getline(inFile, line) ) {
-    cout << line << endl;
+    nl++;
+    if ( nl == 0 ) continue;
+    //cout << line << endl;
+    std::stringstream ss(line);
+    string buf;
+    vector<string> vout;
+    while ( ss >> buf ) vout.push_back(buf);
+    std::reverse(vout.begin(), vout.end());
+    outFile << "Case #" << nl << ":";
+    for (int i = 0; i < vout.size(); ++i ) {
+      outFile << " " << vout[i];
+    }
+    outFile << endl;
   }
   inFile.close();
+  outFile.close();
 
   return 0;
 }
