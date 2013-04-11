d7d17dbcf9c7db3a1006f0017e3518d6b95b05bf
Patrick Huck <phuck@lbl.gov>
2013-04-10 23:35:48 -0700

    move gcj-cl to cl

--- cl/config/user_config.py
+++ cl/config/user_config.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
 'host'                : 'code.google.com',
-'user'                : 'your-name-here@gmail.com',
-'data_directory'      : './source',
+'user'                : 'patrick@the-huck.com',
+'data_directory'      : './src',
 'input_name_format'   : '{problem}-{input}-{id}.in',
 'output_name_format'  : '{problem}-{input}-{id}.out',
-'source_names_format' : [],
+'source_names_format' : ['{problem}-{input}-{id}.cc'],
 }
